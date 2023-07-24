fc6902ff70e446cf4250884c711e02f6d10efd1e
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-07-24 17:22:42 +0200

    feat(views): Add login, register, profile and logout views.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
- 
-function App() {
+import Layout from "./components/layout/layout";
+import Profile from "./components/profile/profile";
+import Login from './components/login/login';
+import Register from './components/register/register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+          <Route path = "/" element = { <Layout /> }>
+            <Route index element = { <Login /> }></Route>
+            <Route path = "/register" element = { <Register /> } ></Route>
+            <Route path = "/profile" element = { <Profile /> }></Route>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
