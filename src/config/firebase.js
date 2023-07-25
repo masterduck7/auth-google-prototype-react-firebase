efb16f32b86b2ac656667c670f9d807b66950461
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-07-25 16:17:50 +0200

    feat(auth): Setup google auth

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,4 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider(app);
 export default app;
